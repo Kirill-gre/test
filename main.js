8e9b51fbba01137bb4ce1e16281831b86a02fbcd
sirilll <kiryane@yandex.ru>
2015-11-21 14:57:40 +0300

    messege

---
+++ main.js
@@ -0,0 +1,17 @@
+
+	$(document).ready(function() {
+		// $('.brick').hide().fadeIn(500);
+		// brick.hide();
+		// brick.fadeIn(500);
+		// console.log(a);
+
+		// $('.brick1').addClass("blue");
+	 //  	$(".brick2").addClass("green");
+	 //  	$(".brick3").addClass("red");
+	// $('.brick2').remove();
+
+  $("#owl-example").owlCarousel();
+
+// $("#burger").on("click",function(e))
+
+	});
